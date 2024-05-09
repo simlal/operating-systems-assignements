be7516707d09c1ee40e1242124b1c55e92b8123f
Simon Lalonde <simlalonde@hotmail.com>
2024-05-09 16:09:48 -0400

    Update with serial ports

--- dev1-rs232-driver/launch-vm.sh
+++ dev1-rs232-driver/launch-vm.sh
@@ -17,12 +17,22 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 if [[ $VM_NUM == 1 ]]; then
     VM_PATH=$SCRIPT_DIR/VM/VM-IFT320-$VM_NUM/Ubuntu-ift320-again-cl$VM_NUM.vmdk
     PORT_NUM=2222
+    SERIAL_CFG_P1=unix:$SCRIPT_DIR/serial0,server,nowait
+    SERIAL_CFG_P2=unix:$SCRIPT_DIR/serial1,server,nowait
 
 elif [[ $VM_NUM == 2 ]]; then
     VM_PATH=$SCRIPT_DIR/VM/VM-IFT320-$VM_NUM/Ubuntu-ift320-again-cl$VM_NUM.vmdk
     PORT_NUM=2223
+    SERIAL_CFG_P1=unix:$SCRIPT_DIR/serial0
+    SERIAL_CFG_P2=unix:$SCRIPT_DIR/serial1
 fi
 
 # Launch the VM with QEMU's ssh support
-qemu-system-x86_64 -hda $VM_PATH -m 1024 -net nic -net user,hostfwd=tcp::$PORT_NUM-:22
+qemu-system-x86_64 \
+    -hda $VM_PATH \
+    -m 1024 \
+    -net nic \
+    -net user,hostfwd=tcp::$PORT_NUM-:22 \
+    -serial $SERIAL_CFG_P1 \
+    -serial $SERIAL_CFG_P2 \
 
