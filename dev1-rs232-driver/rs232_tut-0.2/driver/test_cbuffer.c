c0f5d48ae24b25c3ccf02d78c54cb898c2814ea6
Simon Lalonde <simlalonde@hotmail.com>
2024-05-17 10:23:15 -0400

    test dequeue func and empty/full

--- dev1-rs232-driver/rs232_tut-0.2/driver/test_cbuffer.c
+++ dev1-rs232-driver/rs232_tut-0.2/driver/test_cbuffer.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 #include "cbuffer.h"
 
 int main() {
-    test_enqueue();
+    test_dequeue();
 }
 
 void test_enqueue() {
@@ -27,5 +28,47 @@ void test_enqueue() {
             printf("cb is full!\n");
         }
     }
+    // Free res
     free(data);
+    cbuffer_free(cb);
+}
+
+void test_dequeue() {
+
+    // cbuff of 10 with tail at [1] and head at [3]
+    struct cbuffer* cb = cbuffer_init(8);
+    cb->head = 3;
+    cb->tail = 1;
+
+    cbuffer_info(cb);
+
+    // Fill buffer with bogus
+    u8* data_in;
+    data_in = malloc(20 * sizeof(u8));
+    strcpy(data_in, "helloworld!");
+    int test_len = strlen(data_in);
+
+    for (int i=0; i < test_len; i++) {
+        int enq_status = cbuffer_enqueue(cb, data_in[i]);
+        if (enq_status != 0) {
+            printf("char %c not enqueued, buffer full!\n", data_in[i]);
+        }
+    }
+    cbuffer_info(cb);
+    
+    u8* last_data;
+    last_data = malloc(sizeof(u8));
+    int n_dequeue = 3;
+    printf("dequeuing %i times...\n", n_dequeue);
+    for (int i=0; i < n_dequeue; i++) {
+        cbuffer_dequeue(cb, last_data);
+    }
+    cbuffer_info(cb);
+    printf("last_data dequeued: %s\n", last_data);
+
+    // Free resources
+    free(data_in);
+    free(last_data);
+    cbuffer_free(cb);
+
 }
