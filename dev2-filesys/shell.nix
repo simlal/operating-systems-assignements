816f8e3394de3dbb8c130141acf09f142e06ef33
Simon Lalonde <simlalonde@hotmail.com>
2024-06-08 08:38:00 -0400

    Improve formatting on env enter

--- dev2-filesys/shell.nix
+++ dev2-filesys/shell.nix
@@ -9,6 +9,6 @@ pkgs.mkShell {
         echo -e "Setting up environment...\n"
         echo -e "sshpass available at: $(which sshpass)\n"
 
-        echo "\nNow inside Nix shell with environment ready!"
+        echo -e "Now inside Nix shell with environment ready!"
     '';
 }
