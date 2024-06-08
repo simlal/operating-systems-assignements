5d691f8fc6dbf6790df23ad0c4414110260b6a36
Simon Lalonde <simlalonde@hotmail.com>
2024-06-08 11:27:05 -0400

    Allow single file transfer with

--- dev2-filesys/copy-to-windows.sh
+++ dev2-filesys/copy-to-windows.sh
@@ -21,15 +21,22 @@ FILESYS_DIR_WIN=$MAIN_NACHOS_WIN/filesys
 
 # First copy from deck to win machine
 VM_PASSWORD='ubuntu'
-echo "Copying .h and .cc files from Steam Deck to win machine's user '$WIN_USERNAME'..."
 
-cd $FILESYS_DIR_DECK
-for file in $(ls *.h *.cc); do
-    echo "Copying $file from SteamDeck to win machine..."
-    sshpass -v -p $WIN_ACCOUNT_PASSWORD scp $file $WIN_USERNAME@$WIN_MACHINE_IP:$FILESYS_DIR_WIN/$file
-done
-echo "Files copied successfully."
-
+# Use the $1 if provided to only copy the file otherwise all files
+if [[ $1 ]]; then
+    echo "Copying $1 from SteamDeck to win machine..."
+    sshpass -v -p $WIN_ACCOUNT_PASSWORD scp $FILESYS_DIR_DECK/$1 $WIN_USERNAME@$WIN_MACHINE_IP:$FILESYS_DIR_WIN/$1
+    echo "File copied successfully."
+    exit 0
+else
+    echo "Copying .h and .cc files from Steam Deck to win machine's user '$WIN_USERNAME'..."
+    cd $FILESYS_DIR_DECK
+    for file in $(ls *.h *.cc); do
+        echo "Copying $file from SteamDeck to win machine..."
+        sshpass -v -p $WIN_ACCOUNT_PASSWORD scp $file $WIN_USERNAME@$WIN_MACHINE_IP:$FILESYS_DIR_WIN/$file
+    done
+    echo "Files copied successfully."
+fi
 # Copy from win machine to the vm
 # First ssh into win then scp into each vm
 echo "Call manually './copy-to-vm.sh' from within Git Bash in the win machine."
