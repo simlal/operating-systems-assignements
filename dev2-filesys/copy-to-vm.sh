549bba1bfc007d61936192bafbad96e68d3a1f62
Simon Lalonde <simlalonde@hotmail.com>
2024-06-09 14:52:00 -0400

    Add copy thread-related files in autom script

--- dev2-filesys/copy-to-vm.sh
+++ dev2-filesys/copy-to-vm.sh
@@ -3,10 +3,12 @@
 # Copy the files from WIN MACHINE TO VM
 SCRIPT_DIR=$(pwd)
 FILESYS_DIR_WIN=$SCRIPT_DIR/programming/ift320_dev2/filesys
+THREADS_DIR_WIN=$SCRIPT_DIR/programming/ift320_dev2/threads
 
 # VM credentials and dir
 VM_IP='192.168.59.132'
 FILESYS_DIR_VM='/home/ubuntu/NachosLinuxUdes.IFT320/nachos-tp2-lals2906/code/filesys'
+THREADS_DIR_VM='/home/ubuntu/NachosLinuxUdes.IFT320/nachos-tp2-lals2906/code/threads'
 
 
 # Pre-auth scp from win to vm
@@ -16,6 +18,11 @@ for f in $(ls *.h *.cc); do
     echo "Copying $f from win machine to VM..."
     scp $f ubuntu@$VM_IP:$FILESYS_DIR_VM/$f
 done
+cd $THREADS_DIR_WIN
+echo "Copying 'thread.cc' and 'thread.h' from win machine to VM..."
+scp thread.cc ubuntu@$VM_IP:$FILESYS_DIR_VM/$f
+scp thread.h ubuntu@$VM_IP:$FILESYS_DIR_VM/$f
+
 if [ $? -ne 0 ]; then
     echo "Error copying files to VM."
     exit 1
--- dev2-filesys/copy-to-windows.sh
+++ dev2-filesys/copy-to-windows.sh
@@ -14,18 +14,28 @@ fi
 SCRIPT_DIR=$(pwd)
 MAIN_MACHOS_DECK=$SCRIPT_DIR/nachos
 FILESYS_DIR_DECK=$MAIN_MACHOS_DECK/code/filesys
+THREADS_DIR_DECK=$MAIN_MACHOS_DECK/code/threads
 
 # Directories on WIN machine
 MAIN_NACHOS_WIN=/Users/simla/programming/ift320_dev2/
 FILESYS_DIR_WIN=$MAIN_NACHOS_WIN/filesys
+THREADS_DIR_WIN=$MAIN_NACHOS_WIN/threads
 
 # First copy from deck to win machine
 VM_PASSWORD='ubuntu'
 
 # Use the $1 if provided to only copy the file otherwise all files
 if [[ $1 ]]; then
+    # For threads-related (file startswith 'thread.')
+    if [[ $1 == thread.* ]];then
+        SOURCE_DIR_DECK=$THREADS_DIR_DECK
+        TARGET_DIR_WIN=$THREADS_DIR_WIN
+    else
+        SOURCE_DIR_DECK=$FILESYS_DIR_DECK
+        TARGET_DIR_WIN=$FILESYS_DIR_WIN
+    fi
     echo "Copying $1 from SteamDeck to win machine..."
-    sshpass -v -p $WIN_ACCOUNT_PASSWORD scp $FILESYS_DIR_DECK/$1 $WIN_USERNAME@$WIN_MACHINE_IP:$FILESYS_DIR_WIN/$1
+    sshpass -v -p $WIN_ACCOUNT_PASSWORD scp $SOURCE_DIR_DECK/$1 $WIN_USERNAME@$WIN_MACHINE_IP:$TARGET_DIR_WIN/$1
     echo "File copied successfully."
     exit 0
 else
@@ -35,6 +45,10 @@ else
         echo "Copying $file from SteamDeck to win machine..."
         sshpass -v -p $WIN_ACCOUNT_PASSWORD scp $file $WIN_USERNAME@$WIN_MACHINE_IP:$FILESYS_DIR_WIN/$file
     done
+    cd $THREADS_DIR_DECK
+    echo "Copying 'thread.cc' and 'thread.h' from SteamDeck to win machine..."
+    sshpass -v -p $WIN_ACCOUNT_PASSWORD scp thread.cc $WIN_USERNAME@$WIN_MACHINE_IP:$THREADS_DIR_WIN/thread.cc
+    sshpass -v -p $WIN_ACCOUNT_PASSWORD scp thread.h $WIN_USERNAME@$WIN_MACHINE_IP:$THREADS_DIR_WIN/thread.h
     echo "Files copied successfully."
 fi
 # Copy from win machine to the vm
