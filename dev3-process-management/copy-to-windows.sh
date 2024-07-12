482f056a98b41069870cdf373d5e61892ea40fa9
Simon Lalonde <simlalonde@hotmail.com>
2024-07-12 11:27:09 -0400

    Update scripts for other files to scp

--- dev3-process-management/copy-to-windows.sh
+++ dev3-process-management/copy-to-windows.sh
@@ -15,10 +15,12 @@ fi
 SCRIPT_DIR=$(pwd)
 MAIN_MACHOS_DEB=$SCRIPT_DIR/nachos.tp3
 USERPROG_DIR_DEB=$MAIN_MACHOS_DEB/code/userprog
+THREADS_DIR_DEB=$MAIN_MACHOS_DEB/code/threads
 
 # Directories on WIN machine
 MAIN_NACHOS_WIN=$WIN_USER_PATH/programming/ift320_dev3/
 USERPROG_DIR_WIN=$MAIN_NACHOS_WIN/userprog
+THREADS_DIR_WIN=$MAIN_NACHOS_WIN/threads
 
 # First copy from deb to win machine
 # Use the $1 if provided to only copy the file otherwise all files
@@ -27,20 +29,39 @@ if [[ $1 ]]; then
     if [[ $1 == exception.* ]]; then
         SOURCE_DIR_DEB=$USERPROG_DIR_DEB
         TARGET_DIR_WIN=$USERPROG_DIR_WIN
+    elif [[ $1 == addrspace.* ]]; then
+        SOURCE_DIR_DEB=$USERPROG_DIR_DEB
+        TARGET_DIR_WIN=$USERPROG_DIR_WIN
+    elif [[ $1 == thread.* ]]; then
+        SOURCE_DIR_DEB=$THREADS_DIR_DEB
+        TARGET_DIR_WIN=$THREADS_DIR_WIN
+    else
+        echo "Invalid file name. Exiting..."
+        exit 1
     fi
-    echo "Copying $1 from Deck to win machine..."
-    sshpass -v -p $WIN_ACCOUNT_PASSWORD scp $SOURCE_DIR_DEB/$1 $WIN_USERNAME@$WIN_MACHINE_IP:$TARGET_DIR_WIN/$1
+    echo "Copying $1 from Debian to win machine..."
+    sshpass -v -p "$WIN_ACCOUNT_PASSWORD" scp "$SOURCE_DIR_DEB"/"$1" "$WIN_USERNAME"@$WIN_MACHINE_IP:$TARGET_DIR_WIN/$1
+    if [[ $? -ne 0 ]]; then
+        echo "Error copying file. Exiting..."
+        exit 1
+    fi
     echo "File copied successfully."
     exit 0
 else
-    echo "Copying .h and .cc files from Deb to win machine's user '$WIN_USERNAME'..."
+    echo "Copying 'exception.cc', 'addrspace.cc' and 'addrspace.h' from Debian to win machine..."
     cd "$USERPROG_DIR_DEB" || exit
-    for file in $(ls *.h *.cc); do
-        echo "Copying $file from Deb to win machine..."
-        sshpass -v -p $WIN_ACCOUNT_PASSWORD scp $file $WIN_USERNAME@$WIN_MACHINE_IP:$USERPROG_DIR_WIN/$file
+    for file in exception.cc addrspace.cc addrspace.h; do
+        sshpass -v -p "$WIN_ACCOUNT_PASSWORD" scp $file "$WIN_USERNAME"@"$WIN_MACHINE_IP":"$USERPROG_DIR_WIN"/$file
+    done
+    echo "Files copied successfully."
+
+    echo "Copying thread.cc, thread.h from Debian to win machine..."
+    cd "$THREADS_DIR_DEB" || exit
+    for file in thread.cc thread.h; do
+        sshpass -v -p "$WIN_ACCOUNT_PASSWORD" scp $file "$WIN_USERNAME"@"$WIN_MACHINE_IP":"$THREADS_DIR_WIN"/$file
     done
     # cd $THREADS_DIR_DEB
-    # echo "Copying 'main.cc', 'thread.cc' and 'thread.h' from Deck to win machine..."
+    # echo "Copying 'main.cc', 'thread.cc' and 'thread.h' from Debian to win machine..."
     # sshpass -v -p $WIN_ACCOUNT_PASSWORD scp main.cc $WIN_USERNAME@$WIN_MACHINE_IP:$THREADS_DIR_WIN/main.cc
     # sshpass -v -p $WIN_ACCOUNT_PASSWORD scp thread.cc $WIN_USERNAME@$WIN_MACHINE_IP:$THREADS_DIR_WIN/thread.cc
     # sshpass -v -p $WIN_ACCOUNT_PASSWORD scp thread.h $WIN_USERNAME@$WIN_MACHINE_IP:$THREADS_DIR_WIN/thread.h
@@ -49,5 +70,5 @@ fi
 # Copy from win machine to the vm
 # First ssh into win then scp into each vm
 echo "\"C:\\Program Files\\Git\\bin\\bash.exe\" -c \"./copy-to-vm.sh\""
-sshpass -p $WIN_ACCOUNT_PASSWORD ssh $WIN_USERNAME@$WIN_MACHINE_IP
+# sshpass -p $WIN_ACCOUNT_PASSWORD ssh $WIN_USERNAME@$WIN_MACHINE_IP
 #"C:\Program Files\Git\bin\bash.exe" -c "./copy-to-vm.sh"
